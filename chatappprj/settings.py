13c8046f0d40f9b104e0d7d83fc5f8fc47f8b19f
hammi2 <111009520+hammi2@users.noreply.github.com>
2023-08-24 16:58:06 +0100

    Update settings.py

--- chatappprj/settings.py
+++ chatappprj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c4(@g--#6v_9^2t_tnrkdkdsljgj2so29!@2&(^gio&y&dss-&tsv!-3'
+SECRET_KEY = 'django-insecudsqfre-c4(@g--#6v_9^2t_tnrkdkdsljgj2so29!@2&(^gio&y&dss-&tsv!-3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -28,7 +28,10 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "*",
 ]
-CSRF_TRUSTED_ORIGINS = ["co-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = [
+    "*",
+    "co-production.up.railway.app",
+]
 
 
 # Application definition
